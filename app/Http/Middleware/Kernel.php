e062b7224e733f776090734d8a75f3090df4516f
Le Thanh Dat <lethanhdat1567@gmail.com>
2024-09-22 15:06:21 +0700

    cors config

--- app/Http/Middleware/Kernel.php
+++ app/Http/Middleware/Kernel.php
@@ -19,6 +19,7 @@ class Kernel
     }
     protected $middleware = [
         // Các middleware toàn cục
+        \App\Http\Middleware\Cors::class,
         \App\Http\Middleware\EncryptCookies::class,
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
