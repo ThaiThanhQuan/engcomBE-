689b2d4d10190047777d9da2adc51b9fe320014e
Le Thanh Dat <lethanhdat1567@gmail.com>
2024-10-02 10:44:18 +0700

    Handle comment

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -44,6 +44,7 @@ class CommentController extends Controller
     {
         $comments = Comment::join('users', 'comments.user_id', '=', 'users.id')
             ->where('comments.class_id', $id)
+            ->whereNull('comments.parent_id') 
             ->select('comments.*', 'users.id as user_id', 'users.name as user_name', 'users.avatar as user_avatar')
             ->get();
         
@@ -62,13 +63,14 @@ class CommentController extends Controller
                 ],
             ];
         });
-    
+
         // Đưa ra kết quả
         return response()->json([
             'data' => $formattedComments,
             'message' => 'success'
         ]);
     }
+
     
     
     /**
@@ -105,15 +107,35 @@ class CommentController extends Controller
     public function showResponse(string $id)
     {
         // Lấy tất cả comment có parent_id không null cho class_id cụ thể
-        $comments = Comment::where('class_id', $id)
-            ->whereNotNull('parent_id') // Chỉ lấy comment phản hồi
+        $comments = Comment::join('users', 'comments.user_id', '=', 'users.id')
+            ->where('comments.class_id', $id)
+            ->whereNotNull('comments.parent_id')
+            ->select('comments.*', 'users.id as user_id', 'users.name as user_name', 'users.avatar as user_avatar')
             ->get();
+    
+        // Chuyển đổi dữ liệu để đưa user thông tin vào
+        $formattedComments = $comments->map(function ($comment) {
+            return [
+                'id' => $comment->id,
+                'class_id' => $comment->class_id,
+                'content' => $comment->content,
+                'parent_id' => $comment->parent_id,
+                'created_at' => $comment->created_at,
+                'updated_at' => $comment->updated_at,
+                'user' => [
+                    'id' => $comment->user_id,
+                    'name' => $comment->user_name,
+                    'avatar' => $comment->user_avatar,
+                ],
+            ];
+        });
     
         // Đưa ra kết quả
         return response()->json([
-            'data' => $comments,
+            'data' => $formattedComments,
             'message' => 'success'
         ]);
     }
+    
     
 }
